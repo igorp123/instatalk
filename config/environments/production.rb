64dd63d249b5cc191a31182ed0cb7fcff2ba71b8
Igor <ig.plotnikov@gmail.com>
2019-02-22 16:03:05 +0300

    added heroku config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,8 +43,11 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://minichatrooms.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [
+    'http://minichatrooms.herokuapp.com/cable',
+    'https://minichatrooms.herokuapp.com/cable'
+  ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
